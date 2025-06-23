e22c759af85962412da5dbc2d2378c94f3052419
Kuwon Sebastian Na <laggu91@gmail.com>
2025-06-23 21:44:21 +0900

    feat: collect error for all field (#5)

--- packages/express-cargo/src/types.ts
+++ packages/express-cargo/src/types.ts
@@ -12,3 +12,23 @@ export type ValidatorRule = {
     validate: ValidatorFunction
     message: string
 }
+
+export class CargoFieldError extends Error {
+    name: string
+
+    constructor(property: string | symbol, reason: string) {
+        super(`${String(property)}: ${reason}`)
+        this.name = 'CargoFieldError'
+    }
+}
+
+export class CargoValidationError extends Error {
+    name: string
+    errors: CargoFieldError[]
+
+    constructor(errors: CargoFieldError[]) {
+        super('Cargo validation failed')
+        this.name = 'CargoValidationError'
+        this.errors = errors
+    }
+}
