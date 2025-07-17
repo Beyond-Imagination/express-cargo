0c7c84e2147b18c4195ed07a6752abf5d36f39d9
Kuwon Sebastian Na <laggu91@gmail.com>
2025-07-17 23:11:08 +0900

    feat: source example (#34)

--- apps/example/src/app.ts
+++ apps/example/src/app.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import bodyRouter from './routers/body'
+import sourceRouter from './routers/source'
 
 const app = express()
 
@@ -8,6 +8,6 @@ const port = process.env.PORT || 3000
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use(bodyRouter)
+app.use(sourceRouter)
 
 app.listen(port, () => {console.log(`Example app listening on port ${port}`)})
---
+++ apps/example/src/routers/source.ts
@@ -0,0 +1,58 @@
+import express, { Router } from 'express'
+import { bindingCargo, getCargo, body, query, header, uri } from 'express-cargo'
+
+const router: Router = express.Router()
+
+class BodyExample {
+    @body()
+    number!: number
+
+    @body()
+    string!: string
+
+    @body()
+    boolean!: boolean
+}
+
+router.post('/body', bindingCargo(BodyExample), (req, res) => {
+    const cargo = getCargo<BodyExample>(req)
+    res.json(cargo)
+})
+
+class QueryExample {
+    @query()
+    number!: number
+
+    @query()
+    string!: string
+
+    @query()
+    boolean!: boolean
+}
+
+router.get('/query', bindingCargo(QueryExample), (req, res) => {
+    const cargo = getCargo<QueryExample>(req)
+    res.json(cargo)
+})
+
+class URIExample {
+    @uri()
+    id!: number
+}
+
+router.get('/uri/:id', bindingCargo(URIExample), (req, res) => {
+    const cargo = getCargo<URIExample>(req)
+    res.json(cargo)
+})
+
+class HeaderExample {
+    @header()
+    authorization!: string
+}
+
+router.get('/header', bindingCargo(HeaderExample), (req, res) => {
+    const cargo = getCargo<HeaderExample>(req)
+    res.json(cargo)
+})
+
+export default router
