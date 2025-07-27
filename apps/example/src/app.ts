f5e22a6f3bb04768ea5843d98a023766c42a4a5a
Chewon Kim <stellaa223@gmail.com>
2025-07-27 21:10:28 +0900

    feat: add error handler to example (#49)

--- apps/example/src/app.ts
+++ apps/example/src/app.ts
@@ -1,6 +1,7 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import sourceRouter from './routers/source'
 import validatorRouter from './routers/validator'
+import { CargoValidationError } from 'express-cargo'
 
 const app = express()
 
@@ -11,5 +12,17 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use(sourceRouter)
 app.use(validatorRouter)
+
+app.use((err: unknown, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof CargoValidationError) {
+        return res.status(400).json({
+            errors: err.errors,
+            message: err.message,
+        })
+    }
+    return res.status(500).json({
+        message: 'Internal Server Error',
+    })
+})
 
 app.listen(port, () => {console.log(`Example app listening on port ${port}`)})
