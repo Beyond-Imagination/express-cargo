422e49d400c3c3dce9d9e9aa0ea05e82e9806ae0
laggu91 <laggu91@gmail.com>
2025-07-28 21:54:53 +0900

    reflect review

--- apps/docs/docusaurus.config.ts
+++ apps/docs/docusaurus.config.ts
@@ -19,7 +19,7 @@ const config: Config = {
     url: isVercel ? 'https://dev-docs.express-cargo.beyond-imagination.net/' : 'https://beyond-imagination.github.io',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: isVercel? '/' : '/express-cargo/',
+    baseUrl: isVercel ? '/' : '/express-cargo/',
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
