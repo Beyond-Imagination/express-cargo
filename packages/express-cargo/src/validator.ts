c5b448ef04387450d58ec5e35df31217ead793ab
sezeme <stellaa223@gmail.com>
2025-08-07 18:44:45 +0900

    style: formatting

--- packages/express-cargo/src/validator.ts
+++ packages/express-cargo/src/validator.ts
@@ -69,11 +69,11 @@ export function notEqual(value: any): PropertyDecorator {
 }
 
 export function range(min: number, max: number): PropertyDecorator {
-  return (target, propertyKey) => {
-    addValidator(target, propertyKey, {
-      type: 'range',
+    return (target, propertyKey) => {
+        addValidator(target, propertyKey, {
+            type: 'range',
             validate: (value: any) => typeof value === 'number' && value >= min && value <= max,
-      message: `${String(propertyKey)} must be between ${min} and ${max}`,
-    });
-  };
+            message: `${String(propertyKey)} must be between ${min} and ${max}`,
+        })
+    }
 }
