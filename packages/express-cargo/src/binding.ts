b40b7976892153b39a34522ad2e1e3d44ee9703e
Yoon Jaehyun <yuchem2@gmail.com>
2025-08-13 22:48:31 +0900

    fix: prevent infinite recurisive call

--- packages/express-cargo/src/binding.ts
+++ packages/express-cargo/src/binding.ts
@@ -6,8 +6,6 @@ import { CargoClassMetadata, CargoFieldMetadata } from './metadata'
 export function bindingCargo<T extends object = any>(cargoClass: new () => T): RequestHandler {
     return (req, res, next) => {
         try {
-            const cargo = new cargoClass() as any
-            const classMeta = new CargoClassMetadata(cargoClass.prototype)
             const errors: CargoFieldError[] = []
 
             const getErrorKey = (sourceKey: string, currentKey: string): string => {
@@ -15,12 +13,22 @@ export function bindingCargo<T extends object = any>(cargoClass: new () => T): R
             }
 
             const bindObject = (
-                targetObject: any,
-                metaClass: CargoClassMetadata,
+                objectClass: any,
                 parentSource: string | symbol,
                 sourceData: any,
                 sourceKey: string,
-            ) => {
+                allSource: { body: any, query: any, uri: any, header: any, session: any },
+                visitedObjects: Map<any, any> = new Map(),
+            ): any => {
+                if (visitedObjects.has(objectClass)) {
+                    return visitedObjects.get(objectClass)
+                }
+
+                const metaClass = new CargoClassMetadata(objectClass.prototype)
+                const targetObject = new objectClass()
+
+                visitedObjects.set(objectClass, targetObject)
+
                 const fields = metaClass.getFieldList()
                 for (const property of fields) {
                     const meta: CargoFieldMetadata = metaClass.getFieldMetadata(property)
@@ -42,20 +50,25 @@ export function bindingCargo<T extends object = any>(cargoClass: new () => T): R
                             value = sourceData?.[key]
                             break
                         case 'body':
-                            value = req.body?.[key]
+                            value = allSource.body?.[key]
                             break
                         case 'query':
-                            value = req.query?.[key]
+                            value = allSource.query?.[key]
                             break
                         case 'uri':
-                            value = req.params?.[key]
+                            value = allSource.uri?.[key]
                             break
                         case 'header':
-                            value = req.headers?.[String(key).toLowerCase()]
+                            value = allSource.header?.[String(key).toLowerCase()]
                             break
                         case 'session':
                             value = (req as any).session?.[key]
                             break
+                    }
+
+                    const transformer = meta.getTransformer()
+                    if (transformer) {
+                        value = transformer(value)
                     }
 
                     if (value === undefined || value === null) {
@@ -81,14 +94,9 @@ export function bindingCargo<T extends object = any>(cargoClass: new () => T): R
                         case Date:
                             targetObject[property] = new Date(value)
                             break
-                        default: {
-                            const childClass = meta.type
-                            const childClassMeta = new CargoClassMetadata(childClass.prototype)
-                            const childObject = new childClass()
-                            bindObject(childObject, childClassMeta, currentSource, value, getErrorKey(sourceKey, key))
-                            targetObject[property] = childObject
+                        default:
+                            targetObject[property] = bindObject(meta.type, currentSource, value, getErrorKey(sourceKey, key), allSource)
                             break
-                        }
                     }
 
                     for (const rule of meta.getValidators()) {
@@ -97,8 +105,16 @@ export function bindingCargo<T extends object = any>(cargoClass: new () => T): R
                         }
                     }
                 }
+                return targetObject
             }
-            bindObject(cargo, classMeta, 'initial', null, '')
+            const allSources = {
+                body: req.body,
+                query: req.query,
+                uri: req.params,
+                header: req.headers,
+                session: (req as any).session,
+            }
+            const cargo = bindObject(cargoClass, 'initial', null, '', allSources)
 
             if (errors.length > 0) {
                 throw new CargoValidationError(errors)
