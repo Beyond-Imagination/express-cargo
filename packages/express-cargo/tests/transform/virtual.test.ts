844cb4d146375d9e74f1a1231e9cd545fd6b8353
Jaehyun Yoon <yuchem2@gmail.com>
2025-08-16 12:18:12 +0900

    feat: add virtual decorator (#72)

---
+++ packages/express-cargo/tests/transform/virtual.test.ts
@@ -0,0 +1,51 @@
+import { virtual } from '../../src/transform'
+import { CargoClassMetadata } from '../../src/metadata'
+
+describe('virtual decorator', () => {
+    class Sample {
+        firstName!: string
+        lastName!: string
+        age!: number
+
+        @virtual((obj: Sample) => `${obj.firstName} ${obj.lastName}`)
+        fullName!: string
+
+        @virtual((obj: Sample) => (obj.age >= 18 ? 'Adult' : 'Minor'))
+        ageGroup!: string
+    }
+
+    const classMeta = new CargoClassMetadata(Sample.prototype)
+
+    it('should have virtual metadata for the decorated fields', () => {
+        const fullNameMeta = classMeta.getFieldMetadata('fullName')
+        const ageGroupMeta = classMeta.getFieldMetadata('ageGroup')
+
+        expect(fullNameMeta.getVirtualTransformer()).toBeDefined()
+        expect(ageGroupMeta.getVirtualTransformer()).toBeDefined()
+
+        const firstNameMeta = classMeta.getFieldMetadata('firstName')
+        expect(firstNameMeta.getVirtualTransformer()).toBeUndefined()
+    })
+
+    it('should correctly store the transformer and use the object', () => {
+        const fullNameMeta = classMeta.getFieldMetadata('fullName')
+        const transformer = fullNameMeta.getVirtualTransformer()
+
+        expect(transformer).toBeDefined()
+
+        const testObject = { firstName: 'John', lastName: 'Doe' }
+        expect(transformer!(testObject)).toBe('John Doe')
+    })
+
+    it('should apply the transformation correctly for a single computed field', () => {
+        const ageGroupMeta = classMeta.getFieldMetadata('ageGroup')
+        const transformer = ageGroupMeta.getVirtualTransformer()
+        expect(transformer).toBeDefined()
+
+        const testObjectMinor = { age: 17 }
+        const testObjectAdult = { age: 25 }
+
+        expect(transformer!(testObjectMinor)).toBe('Minor')
+        expect(transformer!(testObjectAdult)).toBe('Adult')
+    })
+})
