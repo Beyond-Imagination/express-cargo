50363684052cd7ec64ba820054729475e2676cbc
sezeme <stellaa223@gmail.com>
2025-08-17 02:43:47 +0900

    feat: add example app for @maxLength decorator

--- apps/example/src/routers/validator.ts
+++ apps/example/src/routers/validator.ts
@@ -1,5 +1,5 @@
 import express, { Router } from 'express'
-import { bindingCargo, getCargo, body, equal, notEqual, prefix, suffix } from 'express-cargo'
+import { bindingCargo, getCargo, body, equal, notEqual, prefix, suffix, maxLength } from 'express-cargo'
 
 const router: Router = express.Router()
 
@@ -76,6 +76,17 @@ class SuffixExample {
 
 router.post('/suffix', bindingCargo(SuffixExample), (req, res) => {
     const cargo = getCargo<SuffixExample>(req)
+    res.json(cargo)
+})
+
+class MaxLengthExample {
+    @body()
+    @maxLength(5)
+    name!: string
+}
+
+router.post('/max-length', bindingCargo(MaxLengthExample), (req, res) => {
+    const cargo = getCargo<MaxLengthExample>(req)
     res.json(cargo)
 })
 
