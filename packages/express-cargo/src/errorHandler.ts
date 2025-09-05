576631dd1aca04e7e38a3c5670719095822ff545
sezeme <stellaa223@gmail.com>
2025-09-05 17:19:01 +0900

    fix : 커스텀 미들웨어 제거 및 null이 아닌 next로 넘어가도록 globalCargoErrorHandler 수정

--- packages/express-cargo/src/errorHandler.ts
+++ packages/express-cargo/src/errorHandler.ts
@@ -3,7 +3,9 @@ import { CargoValidationError } from './types'
 
 export type CargoErrorHandler = (err: CargoValidationError, req: Request, res: Response, next: NextFunction) => void
 
-let globalCargoErrorHandler: CargoErrorHandler | null = null
+let globalCargoErrorHandler = (err: CargoValidationError, req: Request, res: Response, next: NextFunction) => {
+    next(err)
+}
 
 export function setCargoErrorHandler(handler: CargoErrorHandler) {
     globalCargoErrorHandler = handler
@@ -11,24 +13,4 @@ export function setCargoErrorHandler(handler: CargoErrorHandler) {
 
 export function getCargoErrorHandler(): CargoErrorHandler | null {
     return globalCargoErrorHandler
-}
-
-export function cargoErrorMiddleware(err: unknown, req: Request, res: Response, next: NextFunction) {
-    if (err instanceof CargoValidationError) {
-        const handler = getCargoErrorHandler()
-
-        if (handler) {
-            return handler(err, req, res, next)
-        }
-
-        return res.status(400).json({
-            message: err.message,
-            errors: err.errors.map(e => ({
-                name: e.name,
-                message: e.message,
-            })),
-        })
-    }
-
-    next(err)
 }
