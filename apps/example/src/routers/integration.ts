450066c408dc80879ad62a8258afe734d9b87616
Jaehyun Yoon <yuchem2@gmail.com>
2025-10-25 19:16:39 +0900

    feat: add binding exapmle code

---
+++ apps/example/src/routers/integration.ts
@@ -0,0 +1,55 @@
+import express, { Request, Router } from 'express'
+import {
+    body,
+    query,
+    params,
+    validate,
+    header,
+    prefix,
+    transform,
+    request,
+    array,
+    bindingCargo,
+    getCargo,
+} from 'express-cargo'
+
+const router: Router = express.Router()
+
+class PostData {
+    @body()
+    name!: string
+
+    @body()
+    content!: string
+}
+
+class BodyExample {
+    @body()
+    @array(PostData)
+    posts!: PostData[]
+}
+
+class IntegrationExample extends BodyExample {
+    @query()
+    today!: Date
+
+    @params()
+    @validate(value => typeof value === 'number' && value > 0 && value <= 100)
+    case!: number
+
+    @header()
+    @validate(value => value === 'application/json')
+    'content-type'!: string
+
+    @request((request: Request) => request.headers['authorization'])
+    @prefix('Bearer')
+    @transform((value: string) => value.split('Bearer')[1])
+    token!: string
+}
+
+router.post('/integration/:case', bindingCargo(IntegrationExample), (req, res) => {
+    const cargo = getCargo<IntegrationExample>(req)
+    res.json(cargo)
+})
+
+export default router
